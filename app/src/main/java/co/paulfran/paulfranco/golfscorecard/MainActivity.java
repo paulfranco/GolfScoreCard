33361bd05fcca2bc16df29b3d49d0a9f83b378a6
Paul Franco <paulfranco@me.com>
2018-09-05 02:41:11 -0700

    added ListAdapter

--- app/src/main/java/co/paulfran/paulfranco/golfscorecard/MainActivity.java
+++ app/src/main/java/co/paulfran/paulfranco/golfscorecard/MainActivity.java
@@ -6,9 +6,20 @@ import android.os.Bundle;
 
 public class MainActivity extends ListActivity {
 
+    private Hole[] mHoles = new Hole[18];
+    private ListAdapter mListSAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize holes
+        int strokes = 0;
+        for (int i = 0; i < mHoles.length; i++) {
+            mHoles[i] = new Hole("Hole " + (i+1) + " :", strokes);
+        }
+
+        mListSAdapter = new ListAdapter(this, mHoles);
     }
 }
